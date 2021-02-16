dd2dc6b1c6488d4427653b2ec0f1c9f1d206dbf8
asma20 <jbeli.asma20@gmail.com>
2021-02-16 13:39:53 +0100

    teste sexe femme ou homme

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Partie2_Exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            String sexe;
+            Console.WriteLine("Quelle est votre age");
+            age=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Quelle votre sexe");
+            sexe = Console.ReadLine();
+            
+            
+            if (age > 18 && sexe.ToLower() == "homme") {
+                Console.WriteLine("Vous êtes un homme et vous êtes majeur.");
+               }else if (age < 18 && sexe.ToLower() == "homme")
+                {
+                    Console.WriteLine("Vous êtes un homme et vous êtes mineur. ");
+                }
+              else if(age > 18 && sexe.ToLower() == "femme") {
+                Console.WriteLine("Vous êtes une femme et vous êtes majeure.");
+            }
+            else if (age > 18 && sexe.ToLower() == "femme") 
+            {
+                Console.WriteLine("Vous êtes une femme et vous êtes mineure.");
+            }
+
+        }
+    }
+}
